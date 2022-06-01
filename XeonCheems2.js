d4ca550bf6cda95a39508491bb038996739e3615
Hiruna.san <85383228+Hiruna2006@users.noreply.github.com>
2022-06-01 12:56:16 +0530

    Update XeonCheems2.js

--- XeonCheems2.js
+++ XeonCheems2.js
@@ -1,9 +1,9 @@
-//═══════════════════════════════════════════════════════//
+//═════════════════════════════ © 2022 Hiroo.san Bot Inc ══════════════════════════//
 //If you want to recode, reupload
 //or copy the codes/script,
 //pls give credit
 //no credit? i will take action immediately
-//© 2022 Xeon Bot Inc. Cheems Bot MD
+//© 2022 Hiroo.san Bot Inc. Hiroo Bot MD
 //Thank you to Lord Buddha, Family and Myself
 //════════════════════════════//
 require('./settings')
